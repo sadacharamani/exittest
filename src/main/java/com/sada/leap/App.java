0684dd389df77a3e78e0667bbdc2e8e9f70c36aa
sadacharamani <sadacharamani.arulmani@mnscorp.net>
2023-08-16 08:22:38 +0100

    initial commit

---
+++ src/main/java/com/sada/leap/App.java
@@ -0,0 +1,13 @@
+package com.sada.leap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
